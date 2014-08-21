e6545b7bf58d7e2b59e6b2f36f7f18e0ee97d3b8
Mohit <marora3@uic.edu>
2014-08-20 21:25:44 -0500

    LeetCode Excercise

---
+++ LeetCode/ReverseWords.java
@@ -0,0 +1,19 @@
+class ReverseWords {
+    public String reverseWords(String s) {
+	if (s == null || s.length() == 0 || s.equals(" "))
+	    return "";
+	
+	if(s.length() == 1 && !s.equals(" "))
+	    return s;
+	
+ 
+	// Split string
+	String[] strArray = s.split(" ");
+	StringBuilder finalString = new StringBuilder();
+	for (int i = strArray.length - 1; i >= 0; i--) 
+	    if (!strArray[i].equals(""))
+		finalString.append(strArray[i]).append(" ");
+	
+	return finalString.length() == 0 ? "" : finalString.substring(0, finalString.length() - 1);
+    }
+} 
